0319e441f2b8fd7370fed355e7c00816f7cd918a
michaelGit2 <bwibomichael7@gmail.com>
2018-09-14 16:22:29 +0300

    Adds stylesheet and executes javascript

---
+++ js/triangle.js
@@ -0,0 +1,27 @@
+function triangle(){
+var sides = []
+
+sides.push(parseInt(prompt("enter first side")));
+sides.push(parseInt(prompt("enter second side")));
+sides.push(parseInt(prompt("enter third side")));
+
+var first = sides[0];
+var second = sides[1];
+var third = sides[2];
+
+/* console.log(sides); */
+if (first===second  &&  second===third && first===third){
+result.innerHTML=(" It is an Equilateral Triangle")
+}
+
+else if (first===second  &&  first!==third || first===third  &&  first!==second || second===third  &&  second!==first){
+result.innerHTML=("It is an Isosceles Triangle")
+}
+
+else if(first!==second && second!==third && first!==third){
+result.innerHTML=("It is a Scalene Triangle")
+}
+else {
+alert("NOT a triangle")
+}
+}
